6c5cbff49191f106f9d42e14ea0a2c77a77ab8c7
Phuoctran2143 <phuoc.tran23@gmail.com>
2016-09-15 09:29:43 -0600

    first swift playground strings

---
+++ Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var variableString = "hello"
+variableString += " 🌎"
+let multiplier = 3
+let message =  "(multiplier) plus 3"
+
+let string1  = " my name is Phuoc"
+let string2 = " hi, what is your name?"
+let unusualMelnagerie = " dog 🐕,"
+print("ususalMelnagerie has \(unusualMenagerie.characters.count) characters")
+// Print "unusualMenagerie has emogi"
